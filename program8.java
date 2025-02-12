2d1d7ee5f0c60dead37dd6511d674ec42917807f
Sruthikasenthil <shurthydcruze@gmail.com>
2025-02-12 16:02:18 +0530

    Add files via upload

---
+++ program8.java
@@ -0,0 +1,18 @@
+public class main{
+  public static void main(String args[]) {
+
+    int a=86;
+    int b=94;
+    if (true)
+    {
+        System.out.println("a"+"b");
+    }
+  
+     else  
+     {
+       System.out.println( a+b);
+     }
+
+    System.out.println("a"+b);
+    }
+  }
