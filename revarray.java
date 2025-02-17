dba9e8efd276e2f6c575719bf6943f203cc26c3b
Sruthikasenthil <shurthydcruze@gmail.com>
2025-02-17 14:26:23 +0530

    Add files via upload

---
+++ revarray.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6};
+        for (int i=(arr.length-1)/2;i>=0;i--)
+    {
+    System.out.print(arr[i]);
+    }
+    for(int i=arr.length-1;i>=(arr.length)/2;i--)
+    
+{
+   System.out.print(arr[i]);
+}
+}
+}
