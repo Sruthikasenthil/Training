e2720405c9274e7acb70608c75cbe36fc60ebea3
Sruthikasenthil <shurthydcruze@gmail.com>
2025-02-24 10:18:47 +0530

    Add files via upload

---
+++ abstrarea.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Area obj=new Area();
+        obj.calc_area();
+    }
+}
+
+abstract class Circle{
+    protected double area;
+    abstract void calc_area();
+}
+class Area extends Circle
+{
+    private double r;
+    public Area()
+    {
+        Scanner in=new Scanner(System.in);
+        r=in.nextDouble();
+    }
+    void calc_area(){
+        area = 3.14*r*r;
+        System.out.println("Area="+area);
+    }
+}
